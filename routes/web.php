4c9dd6c90a012a003545ff238823286a1cbf605d
Marcelo Cortez <marcelo.cortez@external.engie.com>
2021-05-12 13:52:16 +0100

    create routes, indexes and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProjectController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +24,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::resource('clients', ClientController::class);
+Route::resource('payments', PaymentController::class);
+Route::resource('projects', ProjectController::class);
 
 require __DIR__.'/auth.php';
