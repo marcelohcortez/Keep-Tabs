ee4d528b752355b720488e8a412b9e42e70b116e
Marcelo Cortez <marcelo.cortez@external.engie.com>
2021-05-13 16:25:13 +0100

    list clients

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -16,7 +16,8 @@ class ClientsController extends Controller
      */
     public function index()
     {
-        return view('clients.index');
+        $clients = Client::all();
+        return view('clients.index')->with('clients', $clients);
     }
 
     /**
--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Clients list
+                    @if(count($clients) > 0)
+                        <div class="mt-40">
+                            @foreach($clients as $client)
+                                <div class="shadow-md mt-8 bg-white p-4">
+                                    <h3><a href="/posts/{{$client->id}}">{{$client->company_name}}</a></h3>
+                                    <small>Written on {{$client->created_at}}</small>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
